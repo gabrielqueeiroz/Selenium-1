308d82ebe2c2cd6dc6e324e8e36db8a0fbabe2ec
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2022-10-28 16:39:37 -0400

    update test_home
    Add the quantity of itens per topic and the topic with the highest number of views

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,7 +1,9 @@
 from page_objects.home_page import HomePage
-import pytest
+from page_objects.blog_page import BlogPage
 
-class HomeTest(HomePage):
-    def test_task1(self):
+class HomeTest(HomePage, BlogPage):
+    def test_task2(self):
         self.open_page()
-        self.printer()
+        self.access_blog()
+        self.get_elements()
+        self.show()
